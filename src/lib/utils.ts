df8b34aa5fa27414d6c5c25f2148c45ebf492c5c
CrimsonSpark <zandrewepener012@gmail.com>
2024-07-18 03:37:52 +0200

    Update utils.ts
    Added a function to check if a string is a UUID.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,12 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function isUUID(string: string): boolean {
+  const re =
+    /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+  return re.test(string);
 }
