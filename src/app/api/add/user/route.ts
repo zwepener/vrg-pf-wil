b84f19d88533ddcc14b63722856d862a886a0fc2
CrimsonSpark <zandrewepener012@gmail.com>
2024-07-19 14:36:00 +0200

    Create add/user API route
    To add a user to the database.

---
+++ src/app/api/add/user/route.ts
@@ -0,0 +1,13 @@
+import { NewUserSchema, type NewUser } from "@/lib/definitons";
+import { status401, status501 } from "@/lib/response_codes";
+import { getToken } from "next-auth/jwt";
+import type { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request: NextRequest): Promise<NextResponse> {
+  const token = await getToken({ req: request });
+  if (!token) return status401();
+
+  const rawData = (await request.json()) as NewUser;
+  const { data: parsedData, success, error } = NewUserSchema.safeParse(rawData);
+  return status501();
+}
