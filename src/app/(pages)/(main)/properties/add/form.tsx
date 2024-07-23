8bfdcaab0f36e9f03c548e2a5266b8617ea22131
CrimsonSpark <zandrewepener012@gmail.com>
2024-07-23 10:00:29 +0200

    Partial Form Update

--- src/app/(pages)/(main)/properties/add/form.tsx
+++ src/app/(pages)/(main)/properties/add/form.tsx
@@ -1,28 +1,34 @@
 "use client";
 
-import { RawPropertySchema, type NewProperty } from "@/lib/definitons";
-import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { NewPropertySchema, type NewProperty } from "@/lib/definitons";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { z } from "zod";
 import type { NextResponse } from "next/server";
-
-const formSchema = RawPropertySchema.pick({
-  title: true,
-  description: true,
-  listing_type: true,
-  price: true,
-  bedrooms: true,
-  bathrooms: true,
-  address: true,
-  images: true,
-});
+import { useForm } from "react-hook-form";
 
 export default function AddForm() {
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const form = useForm<NewProperty>({
+    resolver: zodResolver(NewPropertySchema),
     defaultValues: {},
   });
-  async function onSubmit(values: z.infer<typeof formSchema>) {
+  async function onSubmit(values: NewProperty) {
     try {
       const response = (await fetch("/api/property/add", {
         method: "POST",
@@ -37,4 +43,87 @@ export default function AddForm() {
       return;
     }
   }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="title"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Title</FormLabel>
+              <FormControl>
+                <Input placeholder="Enter a property title . . ." {...field} />
+              </FormControl>
+              <FormDescription>
+                This is the title of the property.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="description"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Description</FormLabel>
+              <FormControl>
+                <Input
+                  placeholder="Enter a property description . . ."
+                  {...field}
+                />
+              </FormControl>
+              <FormDescription>
+                This is the desciption of the property.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="listing_type"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Listing Type</FormLabel>
+              <FormControl>
+                <Select>
+                  <SelectTrigger className="w-[180px]">
+                    <SelectValue placeholder="Listing Type" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectItem value="rent">To Let</SelectItem>
+                    <SelectItem value="sell">For Sale</SelectItem>
+                  </SelectContent>
+                </Select>
+              </FormControl>
+              <FormDescription>
+                This is the listing type of the property.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="price"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Price</FormLabel>
+              <FormControl>
+                <Input placeholder="Enter a property price . . ." {...field} />
+              </FormControl>
+              <FormDescription>
+                This is the price of the property.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
 }
