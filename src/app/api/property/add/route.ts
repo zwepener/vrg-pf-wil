63d715efa35e61c88d25e214c977bedf3df140fc
CrimsonSpark <zandrewepener012@gmail.com>
2024-07-25 20:56:46 +0200

    Update property/add API route
    Should be mostly done. Functions as expected with minimal testing.

--- src/app/api/property/add/route.ts
+++ src/app/api/property/add/route.ts
@@ -1,24 +1,78 @@
+import { insertPropertyGetId, updatePropertyBanner } from "@/lib/actions";
 import { NewPropertySchema } from "@/lib/definitons";
-import { status400, status401, status501 } from "@/lib/response_codes";
+import {
+  status201,
+  status400,
+  status401,
+  status500,
+} from "@/lib/response_codes";
+import { put } from "@vercel/blob";
 import { getToken } from "next-auth/jwt";
 import type { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+
+export const AddPropertyAPISchema = NewPropertySchema.extend({
+  banner: z
+    .instanceof(File)
+    .refine(
+      (file) => /\.(gif|jpe?g|tiff?|png|webp|bmp)$/i.test(file.name),
+      "We only support image uploads."
+    )
+    .refine(
+      (file) => file.size <= 1024 * 1024 * 4.5,
+      "File is bigger than 4.5 MB."
+    ),
+});
+export type AddPropertyAPI = z.infer<typeof AddPropertyAPISchema>;
 
 export async function POST(request: NextRequest): Promise<NextResponse> {
   const token = await getToken({ req: request });
   if (!token || token.user.role === "user") return status401();
 
-  const rawData = await request.json();
+  const { id: userId } = token.user;
+
+  const formData = await request.formData();
   const {
-    data: parsedData,
+    data: parsedFormData,
     success,
     error,
-  } = NewPropertySchema.safeParse(rawData);
-  if (!success)
+  } = AddPropertyAPISchema.safeParse({
+    title: formData.get("title"),
+    description: formData.get("description"),
+    listing_type: formData.get("listing_type"),
+    price: parseFloat(formData.get("price") as string),
+    bedrooms: parseInt(formData.get("bedrooms") as string),
+    bathrooms: parseInt(formData.get("bathrooms") as string),
+    address: formData.get("address"),
+    banner: formData.get("banner"),
+  });
+  if (!success) {
     return status400({
       errors: error.flatten().fieldErrors,
       title: "Invalid Form Field Values",
       description: "The server did not receive the data that it expected to.",
     });
-
-  return status501();
+  }
+  try {
+    const { banner, ...payload } = parsedFormData;
+    const propertyId = await insertPropertyGetId({
+      ...payload,
+      agent_id: userId,
+    });
+    const filePath = `properties/${propertyId}/banner.${banner.name
+      .split(".")
+      .pop()}`;
+    const { url: bannerUrl } = await put(filePath, banner, {
+      access: "public",
+      addRandomSuffix: false,
+    });
+    await updatePropertyBanner(propertyId, bannerUrl);
+    return status201({
+      title: "Property Added",
+      description:
+        "A property was successfully added. You might need to refresh the page to see it.",
+    });
+  } catch (error) {
+    return status500();
+  }
 }
