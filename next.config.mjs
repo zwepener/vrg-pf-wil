92ab55a7eda1f0470e1bb89edc558ddce792fbfe
CrimsonSpark <zandrewepener012@gmail.com>
2024-07-25 20:43:38 +0200

    Update next.config.mjs
    Added needed image hosts as per Nextjs requirement.

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "static.vecteezy.com",
+      },
+      {
+        protocol: "https",
+        hostname: "ymv2bzalx31dbsus.public.blob.vercel-storage.com",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
