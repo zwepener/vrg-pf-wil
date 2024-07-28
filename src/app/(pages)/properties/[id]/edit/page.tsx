fb07ab399d3c47ef96004c4a4174c040a134e860
CrimsonSpark <zandrewepener012@gmail.com>
2024-07-28 04:15:03 +0200

    Update properties/[id]/edit dynamic route
    Created the form component that will allow for the editing of Properties.

--- src/app/(pages)/properties/[id]/edit/page.tsx
+++ src/app/(pages)/properties/[id]/edit/page.tsx
@@ -1,14 +1,13 @@
 "use server";
 
 import { fetchProperty } from "@/lib/data";
+import EditForm from "./form";
 
 export default async function Page({ params }: { params: { id: string } }) {
   const property = await fetchProperty(params.id);
   return (
-    <ul>
-      {Object.entries(property).map(([key, value]) => (
-        <li key={key}>{`${key}: ${value}`}</li>
-      ))}
-    </ul>
+    <div className="p-5">
+      <EditForm property={property} />
+    </div>
   );
 }
